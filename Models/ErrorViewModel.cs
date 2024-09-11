41fdb80a0023426ab44ce175adc1fc2511ac87fa
Ceren Ersoz <ersozceren2@gmail.com>
2024-09-11 12:23:29 +0300

    Project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RealtimeMessagingApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
